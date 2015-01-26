97b01d7e70bcc4da9fb4748fa653a9d378ccc77c
Florian Dewulf <floriandewulf@MacBook-Air.local>
2015-01-25 20:09:01 -0500

    finish carou

--- carousel.js
+++ carousel.js
@@ -1,8 +1,8 @@
 window.onload = function() {
   var timeout = undefined;
+  var actif = true;
 
   function changeSlide() {
-    var timeout_tmp = timeout;
     var divs = document.getElementById("caroussel-block").getElementsByTagName("div");
     var index = 0;
 
@@ -21,11 +21,6 @@ window.onload = function() {
     if (index == divs.length)
       index = 0;
     divs[index].className = "active"
-    if (typeof timeout !== "undefined" && timeout_tmp == timeout) {
-      setTimeout(function(){
-        changeSlide();
-      }, 3000);
-    }
   }
 
   function clickEvent() {
@@ -33,6 +28,7 @@ window.onload = function() {
 
     for (var i = 0 ; i < clickable.length ; i++) {
       clickable[i].onclick = function(event) {
+        clearInterval(timeout);
         var nb = event.currentTarget.getAttribute("data-id");
         var divs = document.getElementById("caroussel-block").getElementsByTagName("div");
 
@@ -47,17 +43,55 @@ window.onload = function() {
           }
         }
         divs[nb].className = "active";
+        timeout = setInterval(function(){
+          changeSlide();
+        }, 3000);
       }
     }
-    clearTimeout(timeout);
-    timeout = setTimeout(function(){
-      changeSlide();
-    }, 3600);
   }
 
-  timeout = setTimeout(function(){
+  function eventButton() {
+    var left = document.getElementById("left-arrow");
+    var right = document.getElementById("right-arrow");
+
+    right.onclick = function(event) {
+      clearInterval(timeout);
+      changeSlide();
+      timeout = setInterval(function(){
+        changeSlide();
+      }, 3000);
+    }
+
+    left.onclick = function(event) {
+      clearInterval(timeout);
+      var divs = document.getElementById("caroussel-block").getElementsByTagName("div");
+      var index = 0;
+
+      for (var i = 0 ; i < divs.length ; i++) {
+        if (divs[i].className == "active") {
+          index = i;
+          var move = divs[i];
+          move.className = "moveback";
+          setTimeout(function(){
+            move.className = "";
+          }, 600);
+        }
+      }
+
+      index--;
+      if (index == -1)
+        index = divs.length - 1;
+      divs[index].className = "active"
+      timeout = setInterval(function(){
+        changeSlide();
+      }, 3000);
+    }
+  }
+
+  timeout = setInterval(function(){
     changeSlide();
   }, 3000);
 
   clickEvent();
+  eventButton();
 }

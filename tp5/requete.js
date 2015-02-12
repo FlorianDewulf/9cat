dbc0d7d4cb0bfb7f50b5f4abcac5bac412dbbc17
Ghais BENSAFIA <ghais.bensafia@gmail.com>
2015-02-12 18:31:02 -0500

    change js

--- tp5/requete.js
+++ tp5/requete.js
@@ -1,17 +1,19 @@
 
 var host = "http://127.0.0.1:5000/"
-test = function()
-{
-    console.log("bonjour");
-}
+
+$( document ).ready(function() {
+   getTasks();
+    $("#loading").hide();
+    $("#block_content").show();
+});
 
 getTasks = function() {
     var request = $.ajax({
         url: host + "tasks",
         type: "GET"
     }).done(function (msg) {
+
           console.log(msg);
-        //console.log("ok");
     }).fail(function () {
             alert("error");
         });

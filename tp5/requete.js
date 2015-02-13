76e039f747d3cb1ecace18f75765b5bcf05c7085
Ghais BENSAFIA <ghais.bensafia@gmail.com>
2015-02-12 20:49:51 -0500

    addTaskOk

--- tp5/requete.js
+++ tp5/requete.js
@@ -12,14 +12,20 @@ $(document).ready(function() {
 
     $("#task_create").click(function() {
 
-        if (!$.trim($("input").val()) || !$.trim($("textarea").val())) {
-           // $("#error").fadeIn();
+        var title = $("input").val();
+        var content = $("textarea").val();
+        if (!$.trim(title) || !$.trim(content)) {
 
+            if (!$.trim(title)) {
+                $("#error").text("Le titre ne peut pas etre vide");
+            }
+            else {
+                $("#error").text("Le contenu ne peut pas etre vide");
+            }
             $('#error').fadeIn("slow", function() { $(this).delay(2000).fadeOut("slow"); });
         }
         else {
-            var title = $("input").val();
-            var content = $("textarea").val();
+
             var priority = $('select option:selected').val()
 
             var task = {
@@ -34,14 +40,39 @@ $(document).ready(function() {
 
 });
 
+displayTasks = function (data){
+
+    if ( $("#list").length ) {
+        $("#list").remove();
+    }
+    var list = $("<div id='list'> <div/>");
+    $( "#main_block" ).append(list);
+
+
+    var tmp = data.tasks;
+    tmp.forEach(function (value, index) {
+        var id = tmp[index].id;
+        var content = tmp[index].task.content;
+        var title = tmp[index].task.title;
+        var priority = tmp[index].task.priority;
+
+        var taskHTML = "<div class='task' id='" + id + "'> <dl class='sub-nav'> <dd class='active'><a href='#'>" + title;
+
+        taskHTML += "</a></dd> <dd class='bind-edit'><a href='#'>Editer</a></dd> <dd class='bind-delete'><a href='#'>Supprimer</a></dd>";
+        taskHTML += "<dd class='priority-" + priority + "'></dd>";
+        taskHTML +=  "</dl> <div class='panel panel-override radius'>";
+        taskHTML += "<p>" + content + "</p>" + "<button id='edit_task' task-id='" + id + "'>Editer</button> </div> </div>";
+        $( "#list" ).append(taskHTML);
+    });
+}
 
 getTasks = function() {
     var request = $.ajax({
         url: host + "tasks",
         type: "GET"
     }).done(function (msg) {
-
-          console.log(msg);
+        displayTasks(msg);
+        //  console.log(msg);
     }).fail(function () {
             alert("error");
         });
@@ -60,8 +91,7 @@ addTask = function(task)
         contentType : "application/json",
         data : JSON.stringify(newTask)
     }).done(function (msg) {
-
-        console.log("ok");
+        displayTasks(msg);
     }).fail(function () {
         alert("error");
     });

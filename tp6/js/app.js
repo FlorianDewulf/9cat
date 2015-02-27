49099bed3ba04406f3bea18176b1545a14f4c2b7
Ghais BENSAFIA <ghais.bensafia@gmail.com>
2015-02-27 13:51:24 -0500

    change

--- tp6/js/app.js
+++ tp6/js/app.js
@@ -4,4 +4,34 @@ $(document).ready(function() {
   });
 
   var taskList = new TaskList();
+
+    $("#task_create").click(function() {
+
+        var title = $("input").val();
+        var content = $("textarea").val();
+        if (!$.trim(title) || !$.trim(content)) {
+
+            if (!$.trim(title)) {
+                $("#error").text("Le titre ne peut pas etre vide");
+            }
+            else {
+                $("#error").text("Le contenu ne peut pas etre vide");
+            }
+            $('#error').fadeIn("slow", function() { $(this).delay(2000).fadeOut("slow"); });
+        }
+        else {
+
+            var priority = $('select option:selected').val()
+
+            var task = {
+                title : title,
+                content : content,
+                priority : priority
+            }
+            addTask(task);
+        }
+        $('#new-wrapper input').val("");
+        $('#new-wrapper textarea').val("");
+
+    });
 });

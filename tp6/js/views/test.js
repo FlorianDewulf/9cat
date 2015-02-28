1883840719bf4356cd6c782cca0899e0aa699261
Ghais BENSAFIA <ghais.bensafia@gmail.com>
2015-02-27 21:07:51 -0500

    suppression ok

--- tp6/js/views/test.js
+++ tp6/js/views/test.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
-  TaskList = Backbone.View.extend({
+    TaskList = Backbone.View.extend({
 
-    template: _.template($('#task-template').html()),
+        template: _.template($('#task-template').html()),
 
-    initialize: function(params) {
-      _.bindAll(this, 'render');
-    },
+        events: {
+            'click #delete-task': 'deleteTask'
+        },
 
-    render: function() {
-      this.$el.html(this.template(this.collection.models));
-    }
-  });
+        deleteTask: function(e) {
+            var id = $(e.currentTarget).parent().parent().attr('id');
+
+            for (var i = 0 ; i < this.collection.models.length ; i++) {
+                if (this.collection.models[i].id == id)
+                {
+                    var that = this;
+                    this.collection.models[i].destroy({success : function (model) {
+                        that.collection.remove(model);
+                        that.render();
+                    }
+                    });
+
+                }
+            }
+        },
+
+        initialize: function(params) {
+            _.bindAll(this, 'render');
+            this.collection.on('change', this.render, this);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.collection.models));
+        }
+    });
 
 });

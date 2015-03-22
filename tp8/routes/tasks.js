2e1e0b52238bd3f00738cf6fe3a743ed64e4c642
Florian Dewulf <floriandewulf@MacBook-Air.local>
2015-03-22 10:00:47 -0400

    eljgjglsjf

--- tp8/routes/tasks.js
+++ tp8/routes/tasks.js
@@ -4,9 +4,9 @@ var db = [];
 exports.findAll = function(req, res) {
 	res.send(db);
 };
- 
+
 exports.save = function(req, res) {
-	console.log(req);
+	console.log(req.params);
 	//res.send({id:req.params.id, name: "HURRDURR", description: "description"});
 };
 

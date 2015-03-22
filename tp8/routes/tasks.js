3642ff23a8d481b85c3ef8485d05a40f82bcd7f1
Ghais BENSAFIA <ghais.bensafia@gmail.com>
2015-03-22 10:26:52 -0400

    put ok

--- tp8/routes/tasks.js
+++ tp8/routes/tasks.js
@@ -7,11 +7,18 @@ exports.findAll = function(req, res) {
 exports.save = function(req, res) {
 	var tmp = {id : req.body.id, task : req.body.task };
 	db.push(tmp);
-
 	res.send(tmp);
 };
 
 exports.update = function(req, res) {
+
+	db.forEach(function(task) {
+    if (task.id == req.body.id){
+    	task.id = req.body.id;
+    	task.task = req.body.task;
+    	res.send(task);
+    }
+});
 	//res.send({id:req.params.id, name: "HURRDURR", description: "description"});
 };
 

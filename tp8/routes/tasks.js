c87d7098020600fd03a777c5698f9197daceff16
Ghais BENSAFIA <ghais.bensafia@gmail.com>
2015-03-22 10:15:37 -0400

    fsfdsf

--- tp8/routes/tasks.js
+++ tp8/routes/tasks.js
@@ -1,12 +1,16 @@
 
-var db = [];
 
 exports.findAll = function(req, res) {
-	res.send(db);
+	res.send(req.app.locals.db);
 };
 
 exports.save = function(req, res) {
-	console.log(req.params);
+	// console.log(req.body.id);
+	// console.log(req.body.task);
+
+	var tmp = {id : req.body.id, task : req.body.task };
+	req.app.locals.db.push(tmp);
+	res.send(tmp);
 	//res.send({id:req.params.id, name: "HURRDURR", description: "description"});
 };
 
--- tp8/server.js
+++ tp8/server.js
@@ -6,7 +6,10 @@ var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.locals.db = [];
+
 var tasks = require('./routes/tasks');
+
 
 var corsOptions = {
     origin: '*',
